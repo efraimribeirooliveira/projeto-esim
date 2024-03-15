73457c9d556c7b0e4e6cd265fb07ea53dc73468a
efraimribeirooliveira <efraimribeirooliveira20003131@gmail.com>
2024-03-15 16:00:22 -0300

    Densenvolvimento da barra lateral

---
+++ modelos/index.js
@@ -0,0 +1,19 @@
+const sidebar = document.getElementById('sidebar')
+
+function toggleMenu() {
+    if (sidebar.style.left === '-270px') {
+        sidebar.style.left = '0'
+    } else {
+        sidebar.style.left = '-270px'
+    }
+}
+
+function fecharMenu() {
+    if (sidebar.style.left === '0') {
+        sidebar.style.left = '-270px'
+    } else {
+        sidebar.style.left = '-270px'
+    }
+      
+
+  }
